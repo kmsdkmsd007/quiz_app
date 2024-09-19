4934780b9fdfbac55c87f8910328310039f58c57
Kashif Khan <kashif3d442@gmail.com>
2024-09-19 13:24:36 +0500

    controller and model done

--- lib/modals/quiz_question.dart
+++ lib/modals/quiz_question.dart
@@ -1,10 +1,20 @@
 class QuizQuestion {
-  const QuizQuestion(this.text, this.answer);
+  const QuizQuestion(
+      {required this.answer,
+      required this.text,
+      this.isAnswered = false,
+      required this.correctAnswerIndex});
+  final bool isAnswered;
   final String text;
+  final int correctAnswerIndex;
   final List<String> answer;
-  List<String> getSuffledAnswers() {
-    final shuffled = List.of(answer);
-    shuffled.shuffle();
-    return shuffled;
+
+  QuizQuestion copyWith(bool? isAnswered, String? text, int? correctAnswerIndex,
+      List<String>? answers) {
+    return QuizQuestion(
+        answer: answers ?? answer,
+        text: text ?? this.text,
+        correctAnswerIndex: correctAnswerIndex ?? this.correctAnswerIndex,
+        isAnswered: isAnswered ?? this.isAnswered);
   }
 }
